c91093c1eb9bc5a2bd01147d28da96b4604ea3a6
mikea <michael.appleby@yale.edu>
2016-08-05 14:58:26 -0400

    Format lists and links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+  def render_as_link options={}
+    options[:document] # the original document
+    options[:field] # the field to render
+    options[:value] # the value of the field
+
+    links = []
+    options[:value].each {  |link|
+      links.append(link_to "#{link}", "#{link}")
+    }
+
+    links.join('<br/>').html_safe
+  end
+
+
+  def render_citation options={}
+    citations = []
+    options[:value].each {  |citation|
+      citations.append("<p>" + citation + "</p>")
+    }
+    citations.join(' ').html_safe
+  end
+
+  def render_search_per_line options={}
+    options[:value].each {  |link|
+      links.append(link_to "#{link}", "#{link}")
+    }
+    links.join('<br/>').html_safe
+  end
+
 end
